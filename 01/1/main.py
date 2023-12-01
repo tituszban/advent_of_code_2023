f844e04103cf553c902be52c92a5697d15cbc5b3
Titusz Ban <tituszban@gmail.com>
2023-12-01 09:22:35 +0000

    Day1

---
+++ 01/1/main.py
@@ -0,0 +1,20 @@
+
+def solve(input_lines: list[str]):
+    total = 0
+    for line in input_lines:
+        while not line[0].isdigit():
+            line = line[1:]
+        while not line[-1].isdigit():
+            line = line[:-1]
+        total += int(line[0] + line[-1])
+    return total
+
+def main():
+    with open("01/input.txt") as f:
+        test_input = list(map(lambda l: l.strip(), f.readlines()))
+
+    print(solve(test_input))
+
+
+if __name__ == "__main__":
+    main()
---
+++ 01/2/main.py
@@ -0,0 +1,45 @@
+
+def solve(input_lines: list[str]):
+    total = 0
+    digits = {
+        "1": "1",
+        "2": "2",
+        "3": "3",
+        "4": "4",
+        "5": "5",
+        "6": "6",
+        "7": "7",
+        "8": "8",
+        "9": "9",
+        "0": "0",
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+        "zero": "0",
+    }
+
+    for line in input_lines:
+        while not (digit := next((digit for digit in digits if line.startswith(digit)), None)):
+            line = line[1:]
+        first = digits[digit]
+        while not (digit := next((digit for digit in digits if line.endswith(digit)), None)):
+            line = line[:-1]
+        last = digits[digit]
+        total += int(first + last)
+    return total
+
+def main():
+    with open("01/input.txt") as f:
+        test_input = list(map(lambda l: l.strip(), f.readlines()))
+
+    print(solve(test_input))
+
+
+if __name__ == "__main__":
+    main()
